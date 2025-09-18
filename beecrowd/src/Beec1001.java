77bc4a9336121f899d435e78591fc75f423af5a7
champagnepaxxan <eshenaievasofiya1619@gmail.com>
2025-09-18 21:17:41 +0600

    feat: Beecrowd 1001 ACCEPTED

---
+++ beecrowd/src/Beec1001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Beec1001 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int A = input.nextInt();
+        int B = input.nextInt();
+        int X = A + B;
+        System.out.println("X = " + X);
+        input.close();
+    }
+}
