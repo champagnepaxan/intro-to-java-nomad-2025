0a20bfb8a8590619114096d04b78cd320fe0d5e8
champagnepaxxan <eshenaievasofiya1619@gmail.com>
2025-12-15 18:42:13 +0600

    feat: Beecrowd 1000 ACCEPTED

--- beecrowd/src/Beec1000.java
+++ beecrowd/src/Beec1000.java
@@ -1,3 +1,16 @@
+//public class Beec1000 {
+//    public static void main(String[] args) { System.out.println("Hello World!");}
+//}
+
+import java.util.Scanner;
 public class Beec1000 {
-    public static void main(String[] args) { System.out.println("Hello World!");}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double n1 = sc.nextDouble();
+
+        String n2 = sc.next();
+        System.out.println("N= " + n1 + "N1= "+n1);
+        System.out.println();
+    }
 }
