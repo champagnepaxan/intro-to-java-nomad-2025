a8af32bd72c12933e73527379da011e91a17c958
champagnepaxxan <eshenaievasofiya1619@gmail.com>
2025-12-15 18:49:31 +0600

    feat: Beecrowd 1035 ACCEPTED

---
+++ beecrowd/src/Beec1035.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Beec1035 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        if (b > c && d > a && c > 0 && d > 0 && (c + d) > (a + b) && a % 2 == 0) {
+            System.out.println("Valores aceitos");
+        } else {
+            System.out.println("Valores nao aceitos");
+        }
+    }
+}
