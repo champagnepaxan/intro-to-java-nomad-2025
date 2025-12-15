d8b1c590155c630a06177b90fe60a903cd7c6056
champagnepaxxan <eshenaievasofiya1619@gmail.com>
2025-12-15 18:47:12 +0600

    feat: Beecrowd 1016 ACCEPTED

---
+++ beecrowd/src/Beec1016.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Beec1016 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() + " min");
+    }
+}
