b9c685bc30c78f78e705a0f3cadf3e2978492d0b
champagnepaxxan <eshenaievasofiya1619@gmail.com>
2025-12-15 18:46:45 +0600

    feat: Beecrowd 1014 ACCEPTED

---
+++ beecrowd/src/Beec1014.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Beec1014 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = Integer.valueOf(scanner.nextLine());
+        double y = Double.valueOf(scanner.nextLine());
+
+        System.out.println(String.format("%.3f km/l", x / y));
+    }
+}
